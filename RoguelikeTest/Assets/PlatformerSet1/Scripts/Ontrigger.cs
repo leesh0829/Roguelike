0e77e55d2a60b373779d1022b624b38f9b042971
hinaheadisnotbig <kko20_s23_10102@gclass.ice.go.kr>
2023-12-12 20:53:37 +0900

    Feat: Random Map logic

---
+++ RoguelikeTest/Assets/PlatformerSet1/Scripts/Ontrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Ontrigger : MonoBehaviour
+{
+    public RandomMap RandomMaps;
+    private void OnTriggerEnter2D(Collider2D other) {
+            if(RandomMaps.Room_count < 10) {
+            Debug.Log("닿음");
+            RandomMaps.Nextmap();
+            }
+    }
+
+}
