a1af9b5a735d04033c48184b7c4b2f6d6bbf552f
leesh0829 <ee2hi@naver.com>
2023-12-14 20:03:52 +0900

    aaaa
    aaaaaaaaaaaaaaaaaaaa

--- RoguelikeTest/Assets/Script/Player.cs
+++ RoguelikeTest/Assets/Script/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -8,17 +9,8 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D playerRb;
     private Animator myAnim;
-    public float playerMoveSpeed;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
+    public float move_sp = 0.1f;
+    Vector2 movement = new Vector2();
 
     private void Awake()
     {
@@ -26,11 +18,41 @@ public class Player : MonoBehaviour
         myAnim = GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * playerMoveSpeed * Time.deltaTime;
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        myAnim.SetFloat("MoveX", playerRb.velocity.x);
-        myAnim.SetFloat("MoveY", playerRb.velocity.y);
+        if (movement == new Vector2(0, 1))
+        {
+            transform.Translate(0, move_sp, 0);
+            myAnim.SetFloat("MoveY", move_sp);
+            myAnim.SetFloat("MoveX", 0);
+        }
+        if (movement == new Vector2(0, -1))
+        {
+            transform.Translate(0, -move_sp, 0);
+            myAnim.SetFloat("MoveY", -move_sp);
+            myAnim.SetFloat("MoveX", 0);
+        }
+        if (movement == new Vector2(1, 0))
+        {
+            transform.Translate(move_sp, 0, 0);
+            myAnim.SetFloat("MoveX", move_sp);
+            myAnim.SetFloat("MoveY", 0);
+        }
+        if (movement == new Vector2(-1, 0))
+        {
+            transform.Translate(-move_sp, 0, 0);
+            myAnim.SetFloat("MoveX", -move_sp);
+            myAnim.SetFloat("MoveY", 0);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnim.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * move_sp * Time.deltaTime;
     }
 }
